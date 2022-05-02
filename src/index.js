731c3fe5c089ac8181fe8fbc26c78c78244e474f
Kevin Nielsen <knielsen24@gmail.com>
2022-05-02 15:44:57 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
+const taskForm = document.getElementById('create-task-form')
+const taskDescription = document.getElementById('new-task-description')
+console.log(taskDescription)
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTask (taskDescription.value)
+    taskForm.reset()
+  })
+})
+
+
+///
+
+function addTask (task) {
+  let list = document.createElement('li')
+  let button = document.createElement('button')
+  button.addEventListener('click', handleDelete)
+  button.textContent = 'x'
+  list.textContent = `${task} `
+  list.appendChild(button)
+  document.querySelector('#tasks').appendChild(list)
+}
+
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
